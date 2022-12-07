364a59d40156328b69172025db5a8b9c99746f43
lawanya veer <lawanyaveer@gmail.com>
2022-12-07 14:15:10 +0530

    day 1 project

---
+++ main.py
@@ -0,0 +1,7 @@
+# Band name generator
+# python 100 days bootcamp
+#day 1 project
+
+city =input("what's name of the city you grew up in? ")
+pet = input("what's your pet name?")
+print("your band name could be " + city + pet)
